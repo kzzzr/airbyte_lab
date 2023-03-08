8c83aee4e52fecde56f5c046d626579bc7eb3d86
Artemiy Kzr <34193409+kzzzr@users.noreply.github.com>
2023-03-08 17:58:53 +0000

    Pending changes exported from your codespace

--- variables.tf
+++ variables.tf
@@ -3,12 +3,22 @@ variable "folder_id" {
   type        = string
 }
 
+variable "clickhouse_password" {
+  description = "Clickhouse admin password"
+  type        = string
+  sensitive   = true
+}
+
+output "clickhouse_host_fqdn" {
+  value = resource.yandex_mdb_clickhouse_cluster.clickhouse_starschema.host[0].fqdn
+}
+
 output "yandex_storage_bucket_name" {
   value = resource.yandex_storage_bucket.analytics_engineering_airbyte.id
 }
-output "yandex_storage_bucket_domain_name" {
-  value = resource.yandex_storage_bucket.analytics_engineering_airbyte.bucket_domain_name
-}
+# output "yandex_storage_bucket_domain_name" {
+#   value = resource.yandex_storage_bucket.analytics_engineering_airbyte.bucket_domain_name
+# }
 output "yandex_iam_service_account_static_access_key" {
   value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
 }
@@ -20,17 +30,3 @@ output "yandex_iam_service_account_static_secret_key" {
 output "yandex_compute_instance_nat_ip_address" {
   value = yandex_compute_instance.airbyte.network_interface.0.nat_ip_address
 }
-output "yandex_compute_instance_fqdn" {
-  value = yandex_compute_instance.airbyte.fqdn
-}
-
-
-# variable "clickhouse_password" {
-#   description = "Clickhouse admin password"
-#   type        = string
-#   sensitive   = true
-# }
-
-# output "clickhouse_host_fqdn" {
-#   value = resource.yandex_mdb_clickhouse_cluster.clickhouse_starschema.host[0].fqdn
-# }
