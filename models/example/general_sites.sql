7390038eea25e55bf2853fd0ab1d6a447abdf34b
AlinaSheik05 <136852582+AlinaSheik05@users.noreply.github.com>
2023-06-26 19:38:37 +0300

    Delete models/example directory

--- models/example/general_sites.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-      _airbyte_data['id']::Float64 as id
-    , _airbyte_data['domain'] as domain
-    , _airbyte_data['description'] as description  
-FROM s3('https://storage.yandexcloud.net/{{ env_var("S3_BUCKET_NAME") }}/mybi/general_sites/*'
-    , JSONEachRow
-)
